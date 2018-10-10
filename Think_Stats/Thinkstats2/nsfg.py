d72a5b13364ffd5cba1c17f2387a74c86eaca79d
Tabixe <javierfernandezsuarez@hotmail.com>
2018-10-10 19:21:40 +0100

    Javi Chapter 2-7

--- Think_Stats/Thinkstats2/nsfg.py
+++ Think_Stats/Thinkstats2/nsfg.py
@@ -14,8 +14,8 @@ import thinkstats2
 from collections import defaultdict
 
 
-def ReadFemResp(dct_file='2002FemResp.dct',
-                dat_file='2002FemResp.dat.gz',
+def ReadFemResp(dct_file='../Think_Stats/Thinkstats2/2002FemResp.dct',
+                dat_file='Thinkstats2/2002FemResp.dat.gz',
                 nrows=None):
     """Reads the NSFG respondent data.
 
@@ -38,8 +38,8 @@ def CleanFemResp(df):
     pass
 
 
-def ReadFemPreg(dct_file='2002FemPreg.dct',
-                dat_file='2002FemPreg.dat.gz',
+def ReadFemPreg(dct_file='../Think_Stats/Thinkstats2/2002FemPreg.dct',
+                dat_file='Thinkstats2/2002FemPreg.dat.gz',
 		clean = False):
     """Reads the NSFG pregnancy data.
 
